45170c7679f3aa85d1bd933bc8d10a6daa46117a
Haf <haf@protonmail.ch>
2021-10-24 10:59:00 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Exercises of the book 'Eloquent Javascript'";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in with pkgs;
+  {
+    devShell.${system} = mkShell {
+      nativeBuildInputs = [
+        nodePackages.npm
+      ];
+    };
+  };
+}
