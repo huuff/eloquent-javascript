2a37b5e7273849f684b2c90b2fb4880ecff095f0
Haf <haf@protonmail.ch>
2021-10-26 14:50:30 +0200

    did exercise 1

---
+++ test/projects/c7/exercises.test.js
@@ -0,0 +1,24 @@
+const { VillageState } = require('../../../src/projects/c7/village-state');
+const { runRobot, randomRobot, routeRobot, goalOrientedRobot } = require("../../../src/projects/c7/robots");
+
+const MAX_ITERATIONS = 100;
+
+// Not really a test I guess
+
+test("compare robots", () => {
+  let totalTurnsRandom = 0;
+  let totalTurnsRoute = 0;
+  let totalTurnsGoalOriented = 0;
+
+  for (let i = 0; i < MAX_ITERATIONS; i++) {
+    const villageState = VillageState.random();
+
+    totalTurnsRandom += runRobot(villageState, randomRobot).turns;
+    totalTurnsRoute += runRobot(villageState, routeRobot, []).turns;
+    totalTurnsGoalOriented += runRobot(villageState, goalOrientedRobot, []).turns;
+  }
+
+  console.info(`Average turns for Random Robot: ${totalTurnsRandom / MAX_ITERATIONS}`);
+  console.info(`Average turns for Route Robot: ${totalTurnsRoute / MAX_ITERATIONS}`);
+  console.info(`Average turns for Goal Oriented Robot: ${totalTurnsGoalOriented / MAX_ITERATIONS}`);
+});
