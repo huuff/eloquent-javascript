b071b3092f2ccf9594f61ef67a61fdebdd34ceca
Haf <haf@protonmail.ch>
2021-10-29 18:36:04 +0200

    added routing

--- src/examples/c11/c11.js
+++ src/examples/c11/c11.js
@@ -65,6 +65,42 @@ function sendGossip(nest, message, exceptFor = null) {
   }
 }
 
+function broadcastConnections(nest, name, exceptFor = null) {
+  for (let neighbor of nest.neighbors) {
+    if (neighbor == exceptFor) continue;
+    request(nest, neighbor, "connections", {
+      name,
+      neighbors: nest.state.connections.get(name)
+    });
+  }
+}
+
+function findRoute(from, to, connections) {
+  let work = [{at: from, via: null}];
+
+  for (let i = 0; i < work.length; i++) {
+    let {at, via} = work[i];
+    for (let next of connections.get(at) || []) {
+      if (next == to) return via;
+      if (!work.some(w => w.at == next)) {
+        work.push({at: next, via:via || next});
+      }
+    }
+  }
+
+  return null;
+}
+
+function routeRequest(nest, target, type, content) {
+  if (nest.neighbors.includes(target)) {
+    return request(nest, target, type, content);
+  } else {
+    let via = findRoute(nest.name, target, nest.state.connections);
+    if (!via) throw new Error(`No route to ${target}`);
+    return request(nest, via, "route", { target, type, content });
+  }
+}
+
 function exampleCode() {
   defineRequestType("note", (nest, content, source, done) => {
     console.log(`${nest.name} received note: ${content}`);
@@ -83,7 +119,6 @@ function exampleCode() {
     nest.state.gossip = [];
   });
 
-
   requestType("connections", (nest, { name, neighbors }, source) => {
     let connections = nest.state.connections;
     if (JSON.stringify(connections.get(name)) == JSON.stringify(neighbors)) return;
@@ -91,15 +126,10 @@ function exampleCode() {
     broadcastConnections(nest, name, source);
   });
 
-  function broadcastConnections(nest, name, exceptFor = null) {
-    for (let neighbor of nest.neighbors) {
-      if (neighbor == exceptFor) continue;
-      request(nest, neighbor, "connections", {
-        name,
-        neighbors: nest.state.connections.get(name)
-      });
-    }
-  }
+  requestType("route", (nest, { target, type, content }) => {
+    return routeRequest(nest, target, type, content);
+  });
+
 
   everywhere(nest => {
     nest.state.connections = new Map;
