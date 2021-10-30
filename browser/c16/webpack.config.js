b2d6a163e586774fc4562fec2168e6b1f9ddbd0b
Haf <haf@protonmail.ch>
2021-10-30 18:59:15 +0200

    using webpack for c16

---
+++ browser/c16/webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+  },
+
+  devServer: {
+    static: path.join(__dirname, "dist"),
+  },
+
+  plugins: [new HtmlWebpackPlugin({
+    template: 'src/index.html'
+  })],
+};
