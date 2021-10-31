cba5be14d99b6bf2310a9e6781f7585b7c950166
Haf <haf@protonmail.ch>
2021-10-31 11:25:46 +0100

    adding actor update

--- browser/c16/src/actors.js
+++ browser/c16/src/actors.js
@@ -15,6 +15,10 @@ export class Vec {
   }
 }
 
+const playerXSpeed = 7;
+const gravity = 30;
+const jumpSpeed = 17;
+
 export class Player {
   constructor(pos, speed) {
     this.pos = pos;
@@ -27,6 +31,35 @@ export class Player {
 
   static create(pos) {
     return new Player(pos.plus(new Vec(0, -0.5), new Vec(0, 0)));
+  }
+
+  update(time, state, keys) {
+    let xSpeed = 0;
+    if (keys.ArrowLeft) {
+      xSpeed -= playerXSpeed;
+    }
+    if (keys.ArrowRight) {
+      xSpeed += playerXSpeed;
+    }
+
+    let pos = this.pos;
+    let movedX = pos.plus(new Vec(xSpeed * time, 0));
+
+    if (!state.level.touches(movedX, this.size, "wall")) {
+      pos = movedX;
+    }
+
+    let ySpeed = this.speed.y + time * gravity;
+    let movedY = pos.plus(new Vec(0, ySpeed * time));
+    if (!state.level.touches(movedY, this.size, "wall")) {
+      pos = movedY;
+    } else if (keys.ArrowUp && ySpeed > 0) {
+      ySpeed = -jumpSpeed;
+    } else {
+      ySpeed = 0;
+    }
+
+    return new Player(pos, new Vec(xSpeed, ySpeed));
   }
 }
 
@@ -56,9 +89,22 @@ export class Lava {
   collide(state) {
     return new State(state.level, state.actors, "lost");
   }
+
+  update(time, state) {
+    let newPos = this.pos.plus(this.speed.times(time));
+    if (!state.level.touches(newPos, this.size, "wall")) {
+      return new Lava(newPos, this.speed, this.reset);
+    } else if (this.reset) {
+      return new Lava(this.reset, this.speed, this.reset);
+    } else {
+      return new Lava(this.pos, this.speed.times(-1));
+    }
+  }
 }
 
 Lava.prototype.size = new Vec(1, 1);
+
+const wobbleSpeed = 8, wobbleDist = 0.07;
 
 export class Coin {
   constructor(pos, basePos, wobble) {
@@ -85,6 +131,12 @@ export class Coin {
     }
 
     return new State(state.level, filtered, status);
+  }
+
+  update(time) {
+    let wobble = this.wobble + time * wobbleSpeed;
+    let wobblePos = Math.sin(wobble) * wobbleDist;
+    return new Coin(this.basePos.plus(new Vec(0, wobblePos)), this.basePos, wobble);
   }
 }
 
