11090b5c12e0241b1ee723029ee6eab855dda9ed
Haf <haf@protonmail.ch>
2021-11-15 15:40:28 +0100

    finish grep

--- node/grep/grep.js
+++ node/grep/grep.js
@@ -1,13 +1,19 @@
-const { readFile } = require("fs").promises;
-const { stat } = require("fs").promises;
+const { readFile, stat, readdir } = require("fs").promises;
+const { resolve } = require('path');
 
 const FIRST_ARG_POSITION = 2;
 
 const regex = new RegExp(".*" + process.argv[FIRST_ARG_POSITION] + ".*");
 const args = process.argv.slice(FIRST_ARG_POSITION + 1);
 
-function grepFile(path) {
-    readFile(path, "utf8")
+async function grepDir(path) {
+  for (const file of await readdir(path)) {
+    grepFile(resolve(process.cwd() + `/${path}`, file)); 
+  }
+}
+
+async function grepFile(path) {
+    return readFile(path, "utf8")
       .then(text => {
         for (const line of text.split("\n")) {
           if (line.match(regex)) {
@@ -23,6 +29,8 @@ for (const arg of args) {
     .then(isDirectory => {
       if (!isDirectory) {
         grepFile(arg);
-      }  
+      } else {
+        grepDir(arg);
+      }
     })
 }
